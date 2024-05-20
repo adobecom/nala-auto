b538ea7ffbc73ef0f8909c6b79b704a091e4218d
Jacky Sun <XIASUN@ADOBE.COM>
2024-05-19 22:20:59 -0700

    remove cert

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import fs from 'fs';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
   plugins: [react()],
   server: {
-    https: {
-      key: fs.readFileSync(path.resolve('.', 'key.pem')),
-      cert: fs.readFileSync(path.resolve('.', 'cert.pem')),
-    },
+    // https: {
+    //   key: fs.readFileSync(path.resolve('.', 'key.pem')),
+    //   cert: fs.readFileSync(path.resolve('.', 'cert.pem')),
+    // },
     proxy: {
       '/api': {
         target: 'https://s3-sj3.corp.adobe.com',
