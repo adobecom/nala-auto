23f13fa7c1f5d5eb3ee8371630df0d50f5c76d5e
Jacky Sun <XIASUN@ADOBE.COM>
2024-05-19 22:27:58 -0700

    remove cert

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,6 @@ export default defineConfig({
   base: './',
   plugins: [react()],
   server: {
-    // https: {
-    //   key: fs.readFileSync(path.resolve('.', 'key.pem')),
-    //   cert: fs.readFileSync(path.resolve('.', 'cert.pem')),
-    // },
     proxy: {
       '/api': {
         target: 'https://s3-sj3.corp.adobe.com',
