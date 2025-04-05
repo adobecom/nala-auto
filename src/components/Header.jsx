8745ee31345bcb875d73f3e26c8f6b0a801fa8cf
Jacky Sun <XIASUN@ADOBE.COM>
2025-04-04 17:34:08 -0700

    update promote buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -84,20 +84,15 @@ const Header = ({ isDarkMode, handleThemeToggle, activeMenu, setActiveMenu }) =>
 
             {/* JSON Viewer Link - Right Column */}
             <div className="flex-1 hover:bg-gray-50 transition-colors duration-150">
-              <Link
-                to={`/json-viewer/${item}`}
-                className="px-4 py-2 text-sm text-gray-700 flex items-center gap-2 group"
-                onClick={(e) => {
-                  e.stopPropagation();
-                  setHoveredMenu(null);
-                  setIsMenuOpen(false);
-                }}
+              <button
+                onClick={(e) => handleJsonView(item, e)}
+                className="px-4 py-2 text-sm text-gray-700 flex items-center gap-2 group w-full"
               >
                 <svg className="w-4 h-4 text-gray-400 group-hover:text-primary transition-colors" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z" />
                 </svg>
                 <span className="group-hover:translate-x-1 transition-transform">JSON View</span>
-              </Link>
+              </button>
             </div>
           </div>
         </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,45 +5,53 @@ import Breadcrumb from '../components/Breadcrumb';
 const menuData = {
   MILOCORE: ['milo', 'caas', 'uar', 'feds'],
   CONSUMER: ['sot', 'homepage', 'dc', 'cc', 'bacom', 'bacom-blog', 'express'],
-  GRAYBOX: ['graybox-homepage', 'graybox-dc', 'graybox-cc', 'graybox-bacom', 'graybox-feds', 'da-bacom-graybox'],
+  GRAYBOX: ['graybox-homepage', 'graybox-dc', 'graybox-cc', 'graybox-bacom', 'graybox-federal', 'da-bacom-graybox'],
   DA: ['da-homepage', 'da-dc', 'da-cc', 'da-bacom', 'da-bacom-blog', 'da-feds']
 };
 
 const SHAREPOINT_BASE = 'https://adobe.sharepoint.com/:x:/r/sites/adobecom/';
+const PROMOTE_SMOKE_FILES_BASE = 'https://adobe.sharepoint.com/sites/adobecom/Shared%20Documents/Forms/AllItems.aspx?id=%2Fsites%2Fadobecom%2FShared%20Documents%2F{directory}%2Dgraybox%2Fpromote%2Dsmoke&viewid=d776cf70%2D9b7e%2D4ab7%2Db9da%2D9e0f8e03a7d2';
 const PROMOTE_BASE = `${SHAREPOINT_BASE}_layouts/15/Doc.aspx?sourcedoc=%7B{id}%7D&file=promote-smoke.xlsx&action=default&mobileredirect=true`;
 const PROMOTE_BATCH_BASE = `${SHAREPOINT_BASE}_layouts/15/Doc.aspx?sourcedoc=%7B{id}%7D&file=promote-batch-2k.xlsx&action=default&mobileredirect=true`;
 const LOC_BASE = `https://adobe.sharepoint.com/sites/adobecom/Shared%20Documents/Forms/AllItems.aspx?newTargetListUrl=%2Fsites%2Fadobecom%2FShared%20Documents&viewpath=%2Fsites%2Fadobecom%2FShared%20Documents%2FForms%2FAllItems%2Easpx&id=%2Fsites%2Fadobecom%2FShared%20Documents%2F{directory}%2Dgraybox%2Fdrafts%2Flocalization%2Fjackyloc&viewid=d776cf70%2D9b7e%2D4ab7%2Db9da%2D9e0f8e03a7d2`;
+const GRAYBOX_CONFIG_BASE = 'https://main--{directory}-graybox--adobecom.aem.page/.milo/graybox-config.json';
 
 const testResultsLinks = {
   'graybox-homepage': {
     promote: PROMOTE_BASE.replace('{id}', 'ED68EF15-FEE9-4E1F-BA59-DAC1E465C645'),
     promotebatch: PROMOTE_BATCH_BASE.replace('{id}', '074FE327-957A-4BEF-B2A9-38D9599A98E6'),
-    loc: LOC_BASE.replace('{directory}', 'homepage')
+    loc: LOC_BASE.replace('{directory}', 'homepage'),
+    promoteFiles: PROMOTE_SMOKE_FILES_BASE.replace('{directory}', 'homepage')
   },
   'graybox-dc': {
     promote: PROMOTE_BASE.replace('{id}', 'F7A2C477-5147-4973-A8C4-A4C26B64F9CD'),
     promotebatch: PROMOTE_BATCH_BASE.replace('{id}', '88A67876-B936-47CC-B9DB-44338E7FC1C6'),
-    loc: LOC_BASE.replace('{directory}', 'dc')
+    loc: LOC_BASE.replace('{directory}', 'dc'),
+    promoteFiles: PROMOTE_SMOKE_FILES_BASE.replace('{directory}', 'dc')
   },
   'graybox-cc': {
     promote: PROMOTE_BASE.replace('{id}', 'F61F81F7-F112-40EA-968F-2FAF01C2938D'),
     promotebatch: PROMOTE_BATCH_BASE.replace('{id}', '609B570C-619C-461C-8668-7AC4C0D4253E'),
-    loc: 'https://adobe.sharepoint.com/sites/adobecom/CC/Forms/AllItems.aspx?newTargetListUrl=%2Fsites%2Fadobecom%2FCC&viewpath=%2Fsites%2Fadobecom%2FCC%2FForms%2FAllItems%2Easpx&id=%2Fsites%2Fadobecom%2FCC%2Fwww%2Dgraybox%2Fdrafts%2Flocalization%2Fjackyloc&viewid=820c5cd2%2Dfd43%2D4244%2D92c4%2Dbe6ed86f524e'
+    loc: 'https://adobe.sharepoint.com/sites/adobecom/CC/Forms/AllItems.aspx?newTargetListUrl=%2Fsites%2Fadobecom%2FCC&viewpath=%2Fsites%2Fadobecom%2FCC%2FForms%2FAllItems%2Easpx&id=%2Fsites%2Fadobecom%2FCC%2Fwww%2Dgraybox%2Fdrafts%2Flocalization%2Fjackyloc&viewid=820c5cd2%2Dfd43%2D4244%2D92c4%2Dbe6ed86f524e',
+    promoteFiles: "https://adobe.sharepoint.com/sites/adobecom/CC/Forms/AllItems.aspx?newTargetListUrl=%2Fsites%2Fadobecom%2FCC&viewpath=%2Fsites%2Fadobecom%2FCC%2FForms%2FAllItems%2Easpx&id=%2Fsites%2Fadobecom%2FCC%2Fwww%2Dgraybox%2Fpromote%2Dsmoke&viewid=820c5cd2%2Dfd43%2D4244%2D92c4%2Dbe6ed86f524e"
   },
   'graybox-bacom': {
     promote: PROMOTE_BASE.replace('{id}', 'BACF3853-3534-48AF-B252-2AAC17F0801D'),
     promotebatch: PROMOTE_BATCH_BASE.replace('{id}', 'D5DAC35A-5828-4DCE-BAC8-364F014D0B27'),
-    loc: LOC_BASE.replace('{directory}', 'bacom')
+    loc: LOC_BASE.replace('{directory}', 'bacom'),
+    promoteFiles: PROMOTE_SMOKE_FILES_BASE.replace('{directory}', 'bacom')
   },
-  'graybox-feds': {
+  'graybox-federal': {
     promote: PROMOTE_BASE.replace('{id}', 'A8238599-7245-462D-8863-BC37CACFAC32'),
     promotebatch: PROMOTE_BATCH_BASE.replace('{id}', 'B140BBB4-AA58-4B3D-94C2-82CC43D673A3'),
-    loc: LOC_BASE.replace('{directory}', 'federal')
+    loc: LOC_BASE.replace('{directory}', 'federal'),
+    promoteFiles: PROMOTE_SMOKE_FILES_BASE.replace('{directory}', 'federal')
   },
   'da-bacom-graybox': {
     promote: `${SHAREPOINT_BASE}da-bacom-promote`,
     promotebatch: PROMOTE_BATCH_BASE.replace('{id}', 'xxxx'),
-    loc: `${SHAREPOINT_BASE}da-bacom-loc`
+    loc: `${SHAREPOINT_BASE}da-bacom-loc`,
+    promoteFiles: PROMOTE_SMOKE_FILES_BASE.replace('{directory}', 'da-bacom')
   }
 };
 
@@ -121,6 +129,22 @@ const HomePage = () => {
                                             href={`/json-viewer/${directory}`}
                                         >
                                             Page Load Check
+                                        </a>
+                                        <a 
+                                            className="btn btn-info w-full" 
+                                            href={GRAYBOX_CONFIG_BASE.replace('{directory}', directory.replace('graybox-', ''))}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            Promote Config
+                                        </a>
+                                        <a 
+                                            className="btn btn-info w-full" 
+                                            href={testResultsLinks[directory]?.promoteFiles}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            Promote Smoke Files
                                         </a>
                                         <a 
                                             className="btn btn-info w-full" 
